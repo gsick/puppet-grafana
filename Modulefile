7553ae1ef33c9ffd4216acc5ec4b02384421d786
gsick <gamaliel.sick@yahoo.fr>
2014-06-30 15:18:24 +0200

    Initial import

---
+++ Modulefile
@@ -0,0 +1,10 @@
+name         'gsick-grafana'
+version      '0.0.1'
+source       'https://github.com/gsick/puppet-grafana.git'
+author       'Gamaliel Sick'
+license      'MIT'
+summary      'Grafana Puppet installation module'
+description  'Puppet module for installing and configuring Grafana'
+project_page 'https://github.com/gsick/puppet-grafana'
+
+dependency   'puppetlabs/stdlib', '>= 4.2.2'
---
+++ manifests/init.pp
@@ -0,0 +1,44 @@
+class grafana (
+  $version         = '1.6.1',
+  $destination_dir = '/usr/local/grafana',
+  $tmp             = '/tmp',
+) {
+
+  validate_string($user)
+  validate_absolute_path($destination_dir)
+  validate_absolute_path($tmp)
+
+  ensure_packages(['wget'])
+
+  file { 'grafana home':
+    ensure => 'directory',
+    path   => $destination_dir,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  exec { 'download grafana':
+    cwd     => $tmp,
+    path    => '/sbin:/bin:/usr/bin',
+    command => "wget http://grafanarel.s3.amazonaws.com/grafana-${version}.tar.gz",
+    creates => "${tmp}/grafana-${version}.tar.gz",
+    require => Package['wget'],
+  }
+
+  exec { 'untar grafana':
+    cwd     => $tmp,
+    path    => '/sbin:/bin:/usr/bin',
+    command => "tar -zxvf grafana-${version}.tar.gz",
+    creates => "${tmp}/grafana-${version}/index.html",
+    require => Exec['download grafana'],
+  }
+
+  exec { 'move to dest':
+    cwd     => $tmp,
+    path    => '/sbin:/bin:/usr/bin',
+    command => "\cp -rfT grafana-${version} ${destination_dir}",
+    creates => "${tmp}/grafana-${version}/index.html",
+    require => [Exec['untar grafana'], File['grafana home']],
+  }
+}
